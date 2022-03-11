ce1c5a096a147e6768be5fc41c4aa673ec84b911
minseojo <64322765+minseojo@users.noreply.github.com>
2022-03-12 01:38:22 +0900

    #2 ResponseBody (StringConverter, JsonConverter)

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,13 +3,55 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "spring!");
+        model.addAttribute("data", "hello spring!");
         return "hello";
+    }
+
+    //ResponseBody 문자
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name; // "hello sping"    name=>spring
+    }
+
+
+    // ResponseBody 객체
+    static class Hello {
+        private String name;
+        private Integer age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam(value = "name", required = false) String name, @RequestParam("age") Integer age) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        hello.setAge(age);
+        return hello;
     }
 }
